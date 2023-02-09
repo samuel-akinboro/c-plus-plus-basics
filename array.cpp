cb411816f5ec6519fa44ded05a59cc82b691df03
victor-akinboro <victorakinboro2003@gmail.com>
2023-02-09 12:35:31 +0100

    for each loop in array

--- array.cpp
+++ array.cpp
@@ -5,6 +5,11 @@ int main() {
   string cars[4] = {"Audi", "BMW", "Benz", "tesla"};
 
   for(int i = 0; i < 4; i++) {
-    cout << cars[i] << endl;
+    cout << cars[i] << " for loop" << endl;
+  }
+
+  // for each
+  for (string j : cars){
+    cout << j << endl;
   }
 }
