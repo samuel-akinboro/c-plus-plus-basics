1f7313d3919b957c7d238a160c14684eea73d2f4
victor-akinboro <victorakinboro2003@gmail.com>
2023-02-09 12:30:33 +0100

    array

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string cars[4] = {"Audi", "BMW", "Benz", "tesla"};
+
+  for(int i = 0; i < 4; i++) {
+    cout << cars[i] << endl;
+  }
+}
