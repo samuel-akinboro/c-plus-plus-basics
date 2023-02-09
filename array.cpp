ab2b4aa307e7ef54c911ffef8b6bfc7eb894d76b
victor-akinboro <victorakinboro2003@gmail.com>
2023-02-09 12:40:52 +0100

    getting the actual size of array(lenght)

--- array.cpp
+++ array.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
   string cars[] = {"Audi", "BMW", "Benz", "tesla"};
 
-  cout << sizeof(cars) << endl;
+  // to get length of array
+  cout << sizeof(cars) / sizeof(string) << endl;
 
   // for each
   for (string j : cars){
